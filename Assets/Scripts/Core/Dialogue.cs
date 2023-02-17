06d993f184abaa341b264f5d89f0a2b802424bf4
Adrian24026 <adrian2402glizniewicz@gmail.com>
2023-02-17 21:21:47 +0100

    Added fridge
    :))

--- Assets/Scripts/Core/Dialogue.cs
+++ Assets/Scripts/Core/Dialogue.cs
@@ -77,6 +77,18 @@ public class Dialogue : MonoBehaviour
             "Access denied"
         });
 
+        dialogue.Add("Beer", new string[] {
+            "Do you want to get a beer"
+        });
+
+        dialogue.Add("BeerChoice1", new string[] {
+            "Yes"
+        });
+
+        dialogue.Add("BeerChoice2", new string[] {
+            "No"
+        });
+
         dialogue.Add("AccessB", new string[] {
             "Access granted"
         });
