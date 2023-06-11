ee6bb82cc293eafd427be66c69eeb5dd56bac286
Adrian24026 <adrian2402glizniewicz@gmail.com>
2023-06-11 20:31:46 +0200

    Fix Bugz
    bigjumptrigger i skok poza mape

--- Assets/BigJumpTrigger.cs
+++ Assets/BigJumpTrigger.cs
@@ -8,11 +8,8 @@ public class BigJumpTrigger : MonoBehaviour
     public GameObject balloon1, balloon2;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        foreach (string name in GameManager.Instance.keys)
-        {
 
-            if (collision.gameObject.name == "Player" && (balloon1.active == false && balloon2.active == false))
-                GameManager.Instance.EndGame("BigJump");
-        }    
+        if (collision.gameObject.name == "Player" && (balloon1.activeInHierarchy == false && balloon2.activeInHierarchy == false))
+            GameManager.Instance.EndGame("BigJump");  
     }
 }
